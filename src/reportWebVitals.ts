1ef68b465f497d3b52dfa58da2ae87164a430780
Doron <doron@example.com>
2025-04-29 23:04:42 +0300

    Revert to dynamic import for web-vitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,12 +1,14 @@
-import * as webVitals from 'web-vitals';
+import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    webVitals.getCLS(onPerfEntry);
-    webVitals.getFID(onPerfEntry);
-    webVitals.getFCP(onPerfEntry);
-    webVitals.getLCP(onPerfEntry);
-    webVitals.getTTFB(onPerfEntry);
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
   }
 };
 
