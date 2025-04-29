46f587e719045553dced484742592332d8132085
Doron <doron@example.com>
2025-04-29 23:02:02 +0300

    Update web-vitals import to use default export

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,12 +1,12 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+import webVitals from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    getCLS(onPerfEntry);
-    getFID(onPerfEntry);
-    getFCP(onPerfEntry);
-    getLCP(onPerfEntry);
-    getTTFB(onPerfEntry);
+    webVitals.getCLS(onPerfEntry);
+    webVitals.getFID(onPerfEntry);
+    webVitals.getFCP(onPerfEntry);
+    webVitals.getLCP(onPerfEntry);
+    webVitals.getTTFB(onPerfEntry);
   }
 };
 
